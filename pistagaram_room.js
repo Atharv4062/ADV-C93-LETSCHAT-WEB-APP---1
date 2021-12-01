9766a66e82ba64c7804acd682018d3662e20e2d7
Atharv4062 <88375467+Atharv4062@users.noreply.github.com>
2021-12-01 21:29:31 +0530

    Add files via upload

---
+++ pistagaram_room.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+      apiKey: "AIzaSyBPOa8A-QTDmVEIicu_TR6QYg2KYtDqXLM",
+      authDomain: "pistagram-4d0d0.firebaseapp.com",
+      databaseURL: "https://pistagram-4d0d0-default-rtdb.firebaseio.com",
+      projectId: "pistagram-4d0d0",
+      storageBucket: "pistagram-4d0d0.appspot.com",
+      messagingSenderId: "46199975268",
+      appId: "1:46199975268:web:7c0a1fa78e6b5ee6fade71"
+    };
+    const app = initializeApp(firebaseConfig);
